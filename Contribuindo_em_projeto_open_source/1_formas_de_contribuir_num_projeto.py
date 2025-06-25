0754fbfa089d7ae7ba83b7610fd0554c7aff440c
Alvaro Ito <alvaro.e.ito@gmail.com>
2025-06-25 15:12:27 -0300

    Create 1_formas_de_contribuir_num_projeto.py

---
+++ Contribuindo_em_projeto_open_source/1_formas_de_contribuir_num_projeto.py
@@ -0,0 +1,14 @@
+'''
+O que é um projeto Open Source?
+É um projeto de código aberto, livre para ser utilizado, modificado e distruibuído. Alguns exemplos são:
+- Linux
+- Roadmap DIO
+
+Formas de contribuir em um projeto Open Source:
+- Relatar algum problmea ou bug
+- Sugerir melhorias ou adição de novos recursos
+- Escrever ou atualizar a documentação
+- Contribuir com o código
+- Divulgar e/ou doar
+
+'''
