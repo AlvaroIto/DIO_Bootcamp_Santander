739b05bd511649b731b43f14d54a52c426bcb1dd
Alvaro Ito <alvaro.e.ito@gmail.com>
2025-08-05 15:39:03 -0300

    banco de dados

---
+++ 18_Banco_de_dados/introducao.py
@@ -0,0 +1,19 @@
+'''
+Objetivo geral
+Aprender os principais conceitos de banco de dados, SQL, comandos, tratamento e junção de dados, com foco em desenvolvimento.
+
+Banco de dados é uma coleção organizada de informações (ou dados) estruturadas, normalmente armazenadas eletronicamente em um sistema de computador.
+
+Tabela São dados estruturados e organizados logicamente em formato de linha e coluna
+
+- Banco de dados relacional: O tipo mais usado atualmente, aramazenando dados estruturados, sendo organizado em tabelas, coom colunas e linhas, que se relacionam entre si.
+Exemplos: SQL Server, Oracle Database, MySQL.
+
+- Banco de dados não relacional: Banco de ddados onded os dados não são armazenaddos em tabela, e sim armazenados de maneira não estruturada ou semi-estruturada.
+Exemplo: mongoDB
+Existem vários tipos: document databases, key-value database, wide-column store, e graph databases
+
+DBMS - Database Management System: É um software utilizaddo para acessar, manipular e monitorar um sistema de banco de dados.
+
+
+'''
